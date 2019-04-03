42aabd783fe69ff11dcea773c9721cd40d95ad6e
zygelon <zygelon@gmail.com>
2019-04-04 01:20:25 +0300

    1 part. 53m

---
+++ Source/Training/Pawns/PlayerPawn.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerPawn.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{ 
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("PawnCollision"));
+	SetRootComponent(PawnCollision);
+
+	PawnMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PawnMesh"));
+	PawnMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
